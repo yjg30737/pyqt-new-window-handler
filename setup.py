e0ecddb3471ff046ffa908b61a995bf98a48c026
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 13:16:35 +0900

    Update setup.py to upload this package to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-new-window-handler',
-    version='0.2.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt QObject type class which operates/handles "New Window" feature',
     url='https://github.com/yjg30737/pyqt-new-window-handler.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.15',
         'pyqt-custom-titlebar-setter>=0.0.1',
